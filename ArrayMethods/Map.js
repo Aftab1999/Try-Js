2bfba690af52e0ef9b9ccaa6e26df741c761e4bc
Aftab-1999 <aftaba@codeplateau.com>
2024-06-27 23:42:55 +0530

    filter & map funtions

--- ArrayMethods/Map.js
+++ ArrayMethods/Map.js
@@ -1,7 +1,37 @@
-const array1 = [1, 4, 9, 16];
+// Double the Numbers:
 
-// Pass a function to map
-const map1 = array1.map((x) => x * 2);
+let numbers = [1, 2, 3, 4, 5];
+let doubledNumbers = numbers.map(num => num * 2);
+console.log("Double the Numbers:", doubledNumbers);  // Output: [2, 4, 6, 8, 10]
 
-console.log(map1);
-// Expected output: Array [2, 8, 18, 32]
+
+// Extract Names from Objects:
+
+let persons = [{ name: 'Alice' }, { name: 'Bob' }, { name: 'Charlie' }];
+let names = persons.map(person => person.name);
+console.log("Extract Names from Objects:", names);  // Output: ['Alice', 'Bob', 'Charlie']
+
+
+// Capitalize First Letter of Strings:
+
+let fruits = ['apple', 'banana', 'cherry'];
+let capitalizedFruits = fruits.map(fruit => fruit.charAt(0).toUpperCase() + fruit.slice(1));
+console.log(" Capitalize First Letter of Strings:", capitalizedFruits);  // Output: ['Apple', 'Banana', 'Cherry']
+
+
+// Convert Strings to Lengths:
+
+let fruitLen = ['apple', 'banana', 'cherry'];
+let fruitLengths = fruitLen.map(fruit => fruit.length);
+console.log("Convert Strings to Lengths:", fruitLengths);  // Output: [5, 6, 6]
+
+
+// Transform Array of Arrays:
+
+let arrays = [[1, 2], [3, 4], [5, 6]];
+let sums = arrays.map(array => array.reduce((a, b) => a + b, 0));
+console.log("Transform Array of Arrays:", sums);  // Output: [3, 7, 11]
+
+
+
+
--- ArrayMethods/filter.js
+++ ArrayMethods/filter.js
@@ -7,52 +7,76 @@
 // console.log(newfarr)
 
 
-let farr = [2, 4, 6, 7, 8];
+// let farr = [2, 4, 6, 7, 8];
 
-let newfass = farr.filter((res) => res > 5);
+// let newfass = farr.filter((res) => res > 5);
 
-console.log(newfass)
+// console.log(newfass)
 
 
-/**
-____________________________
-
- */
+/**____________________________
+ **/
 
 
 
 // 2) Given an array of numbers, return a new array that only includes the even numbers.
 
-// let arr = [4,5,6,7,8];
-// let newArr = arr.filter(function (even){
+// let arr = [4, 5, 6, 7, 8];
+// let newArr = arr.filter(function (even) {
 //     return even % 2 == 0;
 // })
+// console.log(newArr);
+
+// with Arrow functions
+
+// let arr = [4, 5, 6, 7, 8];
+// let newArr = arr.filter((even) => even % 2 == 0);
+
 // console.log(newArr);
 
 // 3) Given an array of strings, return a new array that only includes those that are 5 characters or fewer in length
 
 // let arrLen = ["Aftab", "Azim", "AfnanDanish", "Sahil", "AshishSharma"];
-// let newarrlen = arrLen.filter(function(len){
-//     return len.length>5;
+// let newarrlen = arrLen.filter(function (len) {
+//     return len.length > 5;
 // })
 // console.log(newarrlen)
 
+// with Arrow functions
+
+// let arrLen = ["Aftab", "Azim", "AfnanDanish", "Sahil", "AshishSharma"];
+
+// let newarrlen = arrLen.filter((Len) => (Len.length > 5))
+
+// console.log(newarrlen);
+
+
 // Array Method Using Arrow Function
 
-// let Number = [ 50, 45, 66, 30, 10, 20, 70]
+// let Number = [50, 45, 66, 30, 10, 20, 70]
 
-// let newNumber = Number.filter( num =>{
+// let newNumber = Number.filter(num => {
 //     return num < 50;
 // })
 
 // console.log(newNumber);
 
 
-// let arr = [4,5,6,7,8,3,6,7,10,10,63];
-// let newArr = arr.filter( greater =>{
-//     return greater < 5 ;
+
+
+// let arr = [4, 5, 6, 7, 8, 3, 6, 7, 10, 10, 63];
+// let newArr = arr.filter(greater => {
+//     return greater < 5;
 // })
 // console.log(newArr)
+
+
+// let arr = [4, 5, 6, 7, 8, 3, 6, 7, 10, 10, 63];
+
+// let newArr = arr.filter((greater => greater < 5));
+
+// console.log(newArr);
+
 
 
 
