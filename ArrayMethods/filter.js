2910795d833c18cb1884a5ad02075a4610717144
Aftab-1999 <aftaba@codeplateau.com>
2024-06-27 18:48:38 +0530

    updated code

--- ArrayMethods/filter.js
+++ ArrayMethods/filter.js
@@ -1,17 +1,32 @@
 // 1) Given an array of numbers, return a new array that has only the numbers that are 5 or greater.
 
-// let farr = [4,5,6,7,8];
-// let newfarr = farr.filter(function(greater){
-//     return greater > 5 ;
+// let farr = [4, 5, 6, 7, 8];
+// let newfarr = farr.filter(function (greater) {
+//     return greater > 5;
 // })
 // console.log(newfarr)
+
+
+let farr = [2, 4, 6, 7, 8];
+
+let newfass = farr.filter((res) => res > 5);
+
+console.log(newfass)
+
+
+/**
+____________________________
+
+ */
+
+
 
 // 2) Given an array of numbers, return a new array that only includes the even numbers.
 
 // let arr = [4,5,6,7,8];
 // let newArr = arr.filter(function (even){
 //     return even % 2 == 0;
-// }) 
+// })
 // console.log(newArr);
 
 // 3) Given an array of strings, return a new array that only includes those that are 5 characters or fewer in length
@@ -35,7 +50,7 @@
 
 // let arr = [4,5,6,7,8,3,6,7,10,10,63];
 // let newArr = arr.filter( greater =>{
-//     return greater < 5 ; 
+//     return greater < 5 ;
 // })
 // console.log(newArr)
 
---
+++ fsa-js-demo/index.js
@@ -0,0 +1,136 @@
+// variables: memory(RAM) location
+// temporary 
+// primary, secondary
+// concepts(js,java,c#), syntax
+// data types
+// lang agnostic
+// var age = 10; // number
+// var name = 'abc'; // string
+// var addr = ''; // string 
+// var rollNo = 123; // number
+// var isPassed = true; // true, false
+// var balance = 1000.50; // number
+// // object, NaN, null, undefined
+// // typeof 
+// var y = null;
+// toggle comment: Ctrl + /
+// var x = 10; 
+// console.log(typeof x );
+// x = 'abc';
+// x = true;
+// declaration
+// init
+
+// int x = 10.20;
+// LHS = RHS
+// x = "abc"; // compilation error
+// operators : Arithmatic operator (+,-,*,/,%, ++, --)
+// age = age + 1; // ++age; age++;
+// x+2y = 1
+// +, -
+// 2*4 => 2+2+2+2
+// 4/2 => 4-2-2
+// var x = 10;
+// var y = 2;
+// var res = x%2;
+// console.log(res);
+// object, scalar 
+x = 10;
+var o = {};
+var marks = [60, 60, 70, 89, 90, 100]; // lists
+var subjects = ["m1", "Maths2", "Physics"];
+
+// accessing: marks[0]
+// marks[0] = 100;
+// marks.push(20);
+// marks.pop();
+// marks.splice(2, 1);
+
+// marks.shift();
+// marks.unshift(10);
+// console.log(marks.join(' '));
+// var newArray = marks.slice(0, 3);
+// console.log(marks);
+// console.log(newArray);
+// loops: repeatedly
+// Ctrl+c - kill
+// var i = 1;
+// for (; i <= 10; i++) {
+//     console.log(i);
+// }
+
+// for (var i = 1; i <= 5; i = i + 2) {
+//     console.log(i);
+// }
+
+// init var i = 1; once
+// check condition; xX
+// body; xx
+// increments
+// while
+// for (var i = 1; i <= 10; i++) {
+//     for (var j = 1; j <= 10; j++) {
+//         console.log(i + j);
+//     }
+// }
+// 1+2+3+4+5
+// var n = 4;
+
+// var sum = 0;
+// for (var i = 1; i <= n; i++) {
+//     sum = sum + i;
+// }
+
+// console.log(sum);
+
+// var arr = [1, 2, 3, 4, 5, 6]
+
+// console.log(arr.length);
+// arr.splice()
+// var res = arr.slice(0) // [1,2,3]
+// console.log(arr, res);
+// conditional operators <,>, <=, >=, ==, ===, !==
+// logical operator &&, ||, !
+// bitwise
+// = assignment
+// == equality
+// truthy: true and falsy: false
+// falsy: 0, "", false, NaN, null, undefined
+// everything is a value
+// conditional statements, if else, switch
+// var x = 10;
+// var y = "abc";
+// var o = {};
+// var ar=[1,2,3];
+// var active = false;
+
+// console.log(!active);
+
+// if (marks >= 40 || marks <= 100) {
+//     console.log("Passed");
+// }
+// else {
+//     console.log("Failed");
+// }
+// falsy: 0, "", false, NaN, null, undefined
+// if else if ladder
+
+// write a prg to provide grades for a candidate
+// 450, 6subjects
+// percentage
+// >=70 Distinction
+// >=60 && <69  First class
+// >=50 && <59  Second class
+// >=40 && <49  Thrid class
+// <40 : failed
+
+
+
+
+
+
+
+
+
+
+
